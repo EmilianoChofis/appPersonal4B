ab751172878b3450e9f0db93ea3bb61b598c560d
unknown <20213tn128@utez.edu.mx>
2022-11-10 14:46:07 -0600

    terceroo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'personalapp';
+  // count: number = 0;
+  // step: number = 5;
+  // adding(){
+  //   this.count += 1;
+  // }
+  // minus(){
+  //   this.count -= 1;
+  // }
+
+  // changeCount(){//mio
+  //   this.count+=1
+  //   this.count-=1
+  // }
+
+  // changeCounter(param:number){//del profe
+  //   this.count+=param
+  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
+//importaciones de angular
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
+//importaciones de terceros
 import { AppComponent } from './app.component';
+import { HeroesModule } from './heroes/heroes.module';
+
+
+//importaciones propias
+
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent],
+  imports: [ //implementando otros modulos
+    BrowserModule,
+    HeroesModule,
+    ReactiveFormsModule
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [], //ponemos los servicios
+  bootstrap: [AppComponent] //es el componente con el que arranca/inicializa la aplicación
 })
-export class AppModule { }
+export class AppModule {}
